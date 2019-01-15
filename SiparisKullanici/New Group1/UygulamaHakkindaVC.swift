63e04b56edc68431f2a23f792a728fc88c4a8489
Batuhan Erkol <>
2019-01-15 23:09:58 +0300

    Profil -> UygulamaHakkindaVC eklendi, iletisime basıldığında otomatik mail ekrarnı açılma eklendi.

---
+++ SiparisKullanici/New Group1/UygulamaHakkindaVC.swift
@@ -0,0 +1,28 @@
+//
+//  UygulamaHakkindaVC.swift
+//  SiparisKullanici
+//
+//  Created by Batuhan Erkol on 15.01.2019.
+//  Copyright © 2019 Batuhan Erkol. All rights reserved.
+//
+
+import UIKit
+
+class UygulamaHakkindaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func iletisimClicked(_ sender: Any) {
+        let email = "iletisim@sefimapp.com"
+        if let url = URL(string: "mailto:\(email)") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+
+}
